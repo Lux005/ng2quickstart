6e0ac8a58b4324ae64db5c2907e2be943c591fc0
Dennis Lu <DennisxLu@gmail.com>
2016-12-15 03:18:55 -0800

    Angular2 Tutorial-Heros Editor

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,35 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {APP_BASE_HREF} from '@angular/common';//define the base app dir
 
-import { AppComponent }  from './app.component';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import './rxjs-extensions';
+import { AppComponent } from './app.component';
+import { AppRoutingModule, routedComponents } from './app-routing.module';
+import { HeroService } from './hero.service';
+import { HeroSearchComponent } from './hero-search.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 100 })
+  ],
+  declarations: [
+    AppComponent,
+    HeroSearchComponent,
+    routedComponents
+  ],
+  providers: [
+    {provide: APP_BASE_HREF, useValue : '/' },
+    HeroService
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
